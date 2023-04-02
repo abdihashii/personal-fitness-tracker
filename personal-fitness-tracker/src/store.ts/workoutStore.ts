a18a658c66ff005bf91677d38cad66419de61efd
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-04-01 22:34:29 -0500

    add jotai store and clean up

--- personal-fitness-tracker/src/store.ts/workoutStore.ts
+++ personal-fitness-tracker/src/store.ts/workoutStore.ts
@@ -1,6 +1,70 @@
 import { atom } from "jotai";
-import type { ExerciseType, WorkoutDaysType } from "~/types/workoutTypes";
+import type {
+  WorkoutDayType,
+  WarmupType,
+  WorkoutGroupType,
+  ExerciseType,
+  WorkoutDaysType,
+} from "~/types/workoutTypes";
 
+/*
+  workoutDays: [
+    workoutDay: {
+      name: string,
+      warmups: [],
+      workouts: [],
+    }
+  ]
+*/
+export const workoutDaysAtom = atom<WorkoutDaysType>([]);
+export const newWorkoutDayAtom = atom<WorkoutDayType>({
+  name: "",
+  warmups: [],
+  workouts: [],
+});
+
+/*
+  warmups: [
+    {
+      name: string,
+      reps: string | number,
+      link: string,
+    },
+  ]
+*/
+export const warmupsAtom = atom<WarmupType[]>([]);
+export const newWarmupAtom = atom<WarmupType>({
+  name: "",
+  reps: "",
+  link: "",
+});
+
+/*
+  workouts: [
+    workoutGroup: {
+      name: string,
+      exercises: [],
+    }
+  ]
+*/
+export const workoutGroupsAtom = atom<WorkoutGroupType[]>([]);
+export const newWorkoutGroupAtom = atom<WorkoutGroupType>({
+  name: "",
+  exercises: [],
+});
+
+/*
+  exercises: [
+    {
+      name: string,
+      weight: string,
+      sets: string | number,
+      reps: string | number,
+      link: string,
+    },
+  ]
+*/
+export const workoutsAtom = atom<ExerciseType[]>([]);
 export const newWorkoutAtom = atom<ExerciseType>({
   name: "",
   weight: "",
@@ -9,4 +73,29 @@ export const newWorkoutAtom = atom<ExerciseType>({
   link: "",
 });
 
-export const workoutDaysAtom = atom<WorkoutDaysType>([]);
+/**  
+    workoutDays: [
+      workoutDay: {
+        name: string,
+        warmups: [
+          {
+            name: string,
+            reps: string | number,
+            link: string,
+          },
+        ],
+        workouts: [
+          workoutGroup: {
+            name: string,
+            exercises: [
+              {
+                name: string,
+                sets: number,
+                reps: string | number,
+                link: string,
+              },
+            ],
+          }
+        ]
+      }
+   */
