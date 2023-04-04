48f6e8b707bc67cea2d7cbb4190e7375a48c3a73
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-04-03 22:50:37 -0500

    workouts page

--- personal-fitness-tracker/src/pages/new-workout.tsx
+++ personal-fitness-tracker/src/pages/new-workout.tsx
@@ -1,8 +1,10 @@
 import { atom, useAtom } from "jotai";
-import { NewWorkoutDayForm } from "~/components/newWorkoutDayForm";
-import { NewWarmupForm } from "~/components/newWarmupForm";
-import { NewWorkoutGroupForm } from "~/components/newWorkoutGroupForm";
-import { Success } from "~/components/success";
+// import { NewWorkoutDayForm } from "~/components/steps/newWorkoutDayForm";
+// import { NewWarmupForm } from "~/components/newWarmupForm";
+// import { NewWorkoutGroupForm } from "~/components/newWorkoutGroupForm";
+// import { Success } from "~/components/steps/success";
+import Layout from "~/components/layout";
+import { AddNewWorkoutForm } from "~/components/addNewWorkoutForm";
 
 const stepAtom = atom(1);
 
@@ -11,18 +13,24 @@ const NewWorkout = () => {
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
 
-  switch (step) {
-    case 1:
-      return <NewWorkoutDayForm {...{ nextStep }} />;
-    case 2:
-      return <NewWarmupForm {...{ nextStep, prevStep }} />;
-    case 3:
-      return <NewWorkoutGroupForm {...{ nextStep, prevStep }} />;
-    case 4:
-      return <Success {...{ nextStep, prevStep }} />;
-    default:
-      console.log("default");
-  }
+  // switch (step) {
+  //   case 1:
+  //     return <NewWorkoutDayForm {...{ nextStep }} />;
+  //   case 2:
+  //     return <NewWarmupForm {...{ nextStep, prevStep }} />;
+  //   case 3:
+  //     return <NewWorkoutGroupForm {...{ nextStep, prevStep }} />;
+  //   case 4:
+  //     return <Success {...{ nextStep, prevStep }} />;
+  //   default:
+  //     console.log("default");
+  // }
+
+  return (
+    <Layout>
+      <AddNewWorkoutForm />
+    </Layout>
+  );
 };
 
 export default NewWorkout;
